fc8c65e519cd89be5c2b9381736562776bfca27b
Brian Parks <bparks@picturebooth.co>
2016-02-08 15:24:31 -0700

    Burning through numbers

--- providers/RemoteAuthenticationProvider.php
+++ providers/RemoteAuthenticationProvider.php
@@ -27,7 +27,7 @@ class RemoteAuthenticationProvider implements IAuthenticationProvider
 		$ch = curl_init();
 
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, 'Authorization: Basic '.$basic_auth); 
+		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Basic '.$basic_auth]); 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 		$data = curl_exec($ch);
