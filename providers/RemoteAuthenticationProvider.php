a48f9dbc7a3bcae056b4b5fe2cf6df6c2c461b97
Brian Parks <bparks@picturebooth.co>
2016-02-08 14:44:12 -0700

    Send raw HTTP Basic auth header

--- providers/RemoteAuthenticationProvider.php
+++ providers/RemoteAuthenticationProvider.php
@@ -27,8 +27,7 @@ class RemoteAuthenticationProvider implements IAuthenticationProvider
 		$ch = curl_init();
 
 		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC) ; 
-		curl_setopt($ch, CURLOPT_USERPWD, $basic_auth); 
+		curl_setopt($ch, CURLOPT_HTTPHEADER, 'Authorization: Basic '.$basic_auth); 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
 		$data = curl_exec($ch);
